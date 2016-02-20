fab4718746524fa839cc10b427b030b768e99b92
John Bradshaw <johnbradshaw1991@hotmail.com>
2016-02-20 23:22:34 +0000

    less express

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
-
-//Specify a port
-var port = process.env.port || 8080;
-
-//Serve up files in public folder
-app.use('/', express.static(__dirname + '/dist'));
-
-//Start up the website
-app.listen(port);
-console.log('Listening on port: ', port);
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+ 
+// Serve up public/ftp folder 
+var serve = serveStatic('dist', {'index': ['index.html', 'index.htm']})
+ 
+// Create server 
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+ 
+// Listen 
+server.listen(8080)
